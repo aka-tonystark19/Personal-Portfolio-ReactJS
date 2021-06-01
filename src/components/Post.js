310c9a174ed3e91c61f77570bd23a212ab8d69d2
Aditya Phalod <66676624+aka-tonystark19@users.noreply.github.com>
2021-06-01 05:06:26 -0700

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -25,7 +25,7 @@ export default function Post() {
     return (
         <main className="bg-blue-100 min-h-screen p-12">
             <section className="container mx-auto">
-                <h1 className="text-5xl flex justify-center cursive">Blog Post Page</h1>
+                <h1 className="text-5xl flex justify-center ">Blog Post Page</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts!</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
