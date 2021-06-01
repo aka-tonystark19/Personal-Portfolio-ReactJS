a652272364e33fea7b12490550d6b95aebc922cc
Aditya Phalod <66676624+aka-tonystark19@users.noreply.github.com>
2021-06-01 04:56:42 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react"
-import image from "../homeBG.jpg"
+import image from "../lightBlue-bg.jpg"
 
 export default function Home(){
     return (
         <main>
             <img src={image} alt="Minimalistic Background" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg: pt-64 px-8 ">
-                <h1 className="text-6xl text-red-400 font-fold  leading-none lg:leading-snug home-name ">Hello. This is Aditya. </h1>
+                <h1 className="text-3xl bold text-gray-800 font-bold leading-none lg:leading-snug home-name ">HELLO. THIS IS ADITYA. </h1>
+                {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets7.lottiefiles.com/packages/lf20_c1dtgznx.json" mode="bounce" background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player> */}
             </section>
         </main>
     )
